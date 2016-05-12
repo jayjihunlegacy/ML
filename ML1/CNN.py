7a7f4f3d6ecc4e4facf0004a21097c7f5aa8c877
jayjihun <t080205@gmail.com>
2016-05-13 04:25:55 +0900

    CNN 다 짬.

--- ML1/CNN.py
+++ ML1/CNN.py
@@ -2,7 +2,7 @@ import theano
 import theano.tensor as T
 import numpy
 from theano.tensor.nnet import conv2d
-from theano.tensor.signal import downsample
+from theano.tensor.signal import pool
 
 class ConvPoolLayer(object):
     def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2,2)):
@@ -32,14 +32,14 @@ class ConvPoolLayer(object):
                 rng.uniform(
                     low=-W_bound,
                     high=W_bound,
-                    poolsize=filter_shape
+                    size=filter_shape
                     ),
                 dtype=theano.config.floatX
                 ),
             borrow=True
             )
 
-        b_values = numpy.zeros((filter_shape[0],), module_type=theano.config.floatX)
+        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
         self.b = theano.shared(value=b_values, borrow=True)
 
         conv_out = conv2d(
@@ -49,7 +49,7 @@ class ConvPoolLayer(object):
             input_shape=image_shape
             )
 
-        pooled_out = downsample.max_pool_2d(
+        pooled_out = pool.pool_2d(
             input=conv_out,
             ds=poolsize,
             ignore_border=True
--- ML1/Main.py
+++ ML1/Main.py
@@ -4,7 +4,8 @@ import theano.tensor as T
 import LogisticRegression
 import MultiLayerPerceptron
 import timeit
-rng = numpy.random
+import CNN
+rng = numpy.random.RandomState(23455)
 
 
 def shared_dataset(data_xy):
@@ -243,10 +244,155 @@ def mlp_mnist(dataset):
     print('Optimization complete with best validation score of %f %%, with test performance %f %%'%(best_validation_loss*100, test_score*100))
     print('The code run for %d epochs, with %f epochs/sec' %(epoch, epoch/(end_time-start_time)))
 
+def cnn_mnist(dataset):
+    learning_rate = 0.01
+    n_epochs = 200
+    batch_size = 500
+
+    #nkerns : number of kernels on each layer
+    nkerns = [20,50]
+
+
+    train_set_x, train_set_y = dataset[0]
+    valid_set_x, valid_set_y = dataset[1]
+    test_set_x, test_set_y = dataset[2]
+
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0] // batch_size
+
+    index = T.lscalar()
+    x = T.matrix('x')
+    y = T.ivector('y')
+
+    print('... building the model')
+
+    layer0_input = x.reshape((batch_size, 1, 28, 28))
+
+    layer0 = CNN.ConvPoolLayer(
+        rng,
+        input=layer0_input,
+        image_shape=(batch_size,1,28,28),
+        filter_shape=(nkerns[0],1,5,5),
+        poolsize=(2,2)
+        )
+
+    layer1 = CNN.ConvPoolLayer(
+        rng,
+        input=layer0.output,
+        image_shape=(batch_size,nkerns[0],12,12),
+        filter_shape=(nkerns[1],nkerns[0],5,5),
+        poolsize=(2,2)
+        )
+
+    layer2_input = layer1.output.flatten(2)
+
+    layer2 = MultiLayerPerceptron.HiddenLayer(
+        rng,
+        input=layer2_input,
+        n_in=nkerns[1]*4*4,
+        n_out=500,
+        activation=T.tanh
+        )
+
+    layer3 = LogisticRegression.LogisticRegression(input=layer2.output, n_in=500, n_out=10)
+
+    cost = layer3.NLL(y);
+
+    test_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x : test_set_x[index * batch_size : (index+1) * batch_size],
+            y : test_set_y[index * batch_size : (index+1) * batch_size]
+            }
+        )
+
+    validate_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x : valid_set_x[index * batch_size : (index+1) * batch_size],
+            y : valid_set_y[index * batch_size : (index+1) * batch_size]
+            }
+        )
+
+    params = layer3.params + layer2.params + layer1.params + layer0.params
+
+    grads = T.grad(cost,params)
+
+    updates = [(param_i, param_i - learning_rate * grad_i) for param_i, grad_i in zip(params, grads)]
+
+    train_model = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x : train_set_x[index * batch_size : (index+1) * batch_size],
+            y : train_set_y[index * batch_size : (index+1) * batch_size]
+            }
+        )
+
+    print('... train model')
+    patience = 10000
+    patience_increase = 2
+    improvement_threshold = 0.995
+    validation_frequency = min(n_train_batches, patience//2)
+
+    best_validation_loss = numpy.inf
+    best_iter = 0
+    test_score = 0
+    start_time = timeit.default_timer()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+
+        for minibatch_index in range(n_train_batches):
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            #if iter % 100 == 0:
+            print('training @ iter = ',iter)
+            cost_ij = train_model(minibatch_index)
+
+            if(iter+1)%validation_frequency == 0:
+                validation_losses = [validate_model(i) for i in range(n_valid_batches)]
+                this_validation_loss = numpy.mean(validation_losses)
+                print('epoch %i, minibatch %i/%i, validation error %f %%'%(epoch, minibatch_index+1, n_train_batches, this_validation_loss*100))
+
+                if this_validation_loss < best_validation_loss:
+                    if this_validation_loss < best_validation_loss*improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    test_losses = [test_model(i) for i in range(n_test_batches)]
+                    test_score = numpy.mean(test_losses)
+
+                    print('\tepoch %i, minibatch %i/%i, test error of best model %f %%'%(epoch, minibatch_index+1, n_train_batches, test_score*100))
+
+            if patience <= iter:
+                done_looping = True
+                break
+
+    end_time = timeit.default_timer()
+    print('Optimization complete')
+    print('Best validation score of %f %% obtained at iteration %i, with test performance %f %%'%(best_validation_loss*100, best_iter+1, test_score*100))
+
+
+
+
+
+
+
 def main():
     dataset = get_mnist();
     #sgd_optimization_mnist(dataset)
-    mlp_mnist(dataset)
+    #mlp_mnist(dataset)
+    cnn_mnist(dataset)
 
    
 
